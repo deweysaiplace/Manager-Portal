a35e3e52725857946adce3654fe30fb9a7b8c880
Campaign Manager <campaign@deweysaiplace.com>
2026-03-03 22:18:41 -0600

    Update SW to Network First

--- sw.js
+++ sw.js
@@ -1,15 +1,35 @@
+const CACHE_NAME = 'manager-store-v2';
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Force new service worker to activate immediately
   e.waitUntil(
-    caches.open('manager-store').then((cache) => cache.addAll([
-        './index.html',
-        './manifest.json'
+    caches.open(CACHE_NAME).then((cache) => cache.addAll([
+      './index.html',
+      './manifest.json'
     ])),
   );
 });
 
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+// Network-First Strategy
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
+    fetch(e.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(e.request, response.clone());
+        return response;
+      });
+    }).catch(() => caches.match(e.request))
   );
 });
